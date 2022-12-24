4218036910acaf1048f414c1bda74a794c6d776e
RRocha21 <renatorocha21@ua.pt>
2022-12-24 01:21:45 +0000

    sponsors v0.01

--- pages/[streamerid]/[overlayid].js
+++ pages/[streamerid]/[overlayid].js
@@ -1,13 +1,106 @@
 import Head from 'next/head'
 import { connectToDatabase } from '../../util/mongodb'
 import styled from 'styled-components'
+import Image from 'next/image'
 
-export default function Home({properties }) {
+export default function Home({properties, sponsor_props, overlay_props}) {
 
   if (properties != undefined) {
-
-    properties = [...properties].sort((a, b) => parseFloat(b.css.zindex) - parseFloat(a.css.zindex));
+      properties = [...properties].sort((a, b) => parseFloat(b.css.zindex) - parseFloat(a.css.zindex));
   }
+  var width = 0;
+  var height = 0;
+  var left = 0;
+  var top = 0;
+  var sponsor_sizes = 0;
+  var start_left = 0;
+
+  var new_sponsor_props = [];
+
+  if (sponsor_props != undefined) {
+    sponsor_props = [...sponsor_props].sort((a, b) => parseFloat(a.order) - parseFloat(b.order));
+    if (overlay_props != undefined) {
+      width = overlay_props.sponsor_container.size.width;
+      height = overlay_props.sponsor_container.size.height;
+      left = overlay_props.sponsor_container.position.left;
+      top = overlay_props.sponsor_container.position.top;
+      sponsor_sizes = (overlay_props.sponsor_container.size.width / sponsor_props.length) - 10;
+      sponsor_sizes = sponsor_sizes.toString();
+      console.log(sponsor_sizes);
+    }
+    for (var i = 0; i < sponsor_props.length; i++) {
+      var file_black_path = sponsor_props[i].file.black.path;
+      var file_white_path = sponsor_props[i].file.white.path;
+      if (i == 0) {
+        start_left = 0;
+      } else {
+        start_left = start_left + parseInt(sponsor_sizes);
+      }
+      for (var j = 10000; j>0; j--) {
+        var value = j / 10000;
+        if (sponsor_props[i].file.black.size.width * value < parseInt(sponsor_sizes)) {
+          if (sponsor_props[i].file.black.size.height * value <= height) {
+            var file_black_size_width = value * sponsor_props[i].file.black.size.width;
+            var file_black_size_height = value * sponsor_props[i].file.black.size.height;
+            break;
+          }
+        }
+      }
+      for (var j = 10000; j>0; j--) {
+        var value = j / 10000;
+        if (sponsor_props[i].file.white.size.width * value < parseInt(sponsor_sizes)) {
+          if (sponsor_props[i].file.white.size.height * value <= height) {
+            var file_white_size_width = value * sponsor_props[i].file.white.size.width;
+            var file_white_size_height = value * sponsor_props[i].file.white.size.height;
+            break;
+          }
+        }
+      }
+        var new_set = {
+        file: {
+          black: {
+            path: file_black_path,
+            size: {
+              width: file_black_size_width,
+              height: file_black_size_height,
+              top: (height - file_black_size_height) / 2,
+            }
+          },
+          white: {
+            path: file_white_path,
+            size: {
+              width: file_white_size_width,
+              height: file_white_size_height,
+              top: (height - file_white_size_height) / 2,
+            }
+          }
+        },
+        order: sponsor_props[i].order,
+        css: {
+          left: start_left,
+          top: top,
+          zindex: 500
+        }
+      }
+      // console.log(start_left);
+      new_sponsor_props.push(new_set);
+    }
+  }
+
+  if (new_sponsor_props != undefined) {
+    var total_width = 0;
+    for (var i = 0; i < new_sponsor_props.length; i++) {
+      total_width += new_sponsor_props[i].file.white.size.width;
+    }
+    console.log(total_width)
+    var setWidth = (((width) - total_width) / 2 ) - (10 * (new_sponsor_props.length -1))/2;
+    console.log(setWidth);
+    for (var i = 0; i < new_sponsor_props.length; i++) {
+      new_sponsor_props[i].css.left = setWidth;
+      setWidth += new_sponsor_props[i].file.white.size.width + 10;
+    }
+  }
+
 
   if (properties != undefined) {
     return (
@@ -18,6 +111,13 @@ export default function Home({properties }) {
         </Head>
 
         <div>
+          <div style={{position: "absolute", left: left + "px", top: top + "px", width: width + "px", height: height + "px", background: "#FFA07A", zIndex: "450"}}>
+          {new_sponsor_props.map((new_sponsor_prop) => ( 
+              <div style={{position: "absolute", left: new_sponsor_prop.css.left + "px", top: "0px", width: new_sponsor_prop.file.white.size.width + "px", height: height + "px"}}>
+                <img src= {"https://g2layer-4sknz.ondigitalocean.app/" + new_sponsor_prop.file.white.path} style={{ alignSelf: "center", position: "absolute", left: "0px", top: new_sponsor_prop.file.white.size.top + "px", width: new_sponsor_prop.file.white.size.width + "px", height: new_sponsor_prop.file.white.size.height + "px", zIndex:"500"}}/>
+              </div>
+          ))}
+          </div>
           {properties.map((property) => (
             <div>
               <img src= {"https://g2layer-4sknz.ondigitalocean.app/" + property.file.path} style={{position: "absolute", left: property.css.left + "px", top: property.css.top + "px", zIndex: property.css.zindex}}/>
@@ -41,14 +141,33 @@ export async function getStaticPaths() {
 export async function getStaticProps({ params }) {
     const { db } = await connectToDatabase();
 
-    console.log(params)
     const data = await db.collection("streamers").find({twitch_id: params.streamerid}).toArray();
 
     const streamers = await db.collection('streamers').find({}).toArray();
     const staticOverlays = await db.collection('staticoverlays').find({}).toArray();
     const staticArts = await db.collection('staticarts').find({}).toArray();
-  
+    const groups = await db.collection('groups').find({}).toArray();
+    const sponsors = await db.collection('sponsors').find({}).toArray();
+
     var file = [];
+    var sponsor_array = [];
+  
+    for (const streamer of streamers) {
+      if (streamer.twitch_id == params.streamerid) {
+        for (const group of groups) {
+          if (streamer.group == group.name) {
+            for (const sponsor of sponsors) {
+              for (const group_sponsor of group.sponsors) {
+                if (sponsor.name == group_sponsor) {
+                  sponsor_array.push(sponsor);
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+
     for (const staticOverlay of staticOverlays) {
       for (const staticOverlay_on_streamer of staticOverlay.on.streamer) {
         if (staticOverlay_on_streamer == params.streamerid) {
@@ -61,17 +180,24 @@ export async function getStaticProps({ params }) {
                 }
               }
             }
-          
         }
       }
     }
 
-    // console.log(file);
+    var overlay_array = [];
+    for (const staticOverlay of staticOverlays) {
+      if (staticOverlay.OverlayName == params.overlayid) {
+        overlay_array = staticOverlay;
+      }
+    }
 
+    // console.log(file);
+    const overlay_props = JSON.parse(JSON.stringify(overlay_array));
+    const sponsor_props = JSON.parse(JSON.stringify(sponsor_array));
     const properties = JSON.parse(JSON.stringify(file));
 
     return {
-        props: {properties: properties },
+        props: {properties: properties, sponsor_props: sponsor_props, overlay_props: overlay_props },
         revalidate: 1
     };
 }
--- pages/[streamerid]/campaigns.js
+++ pages/[streamerid]/campaigns.js
@@ -3,27 +3,69 @@ import { connectToDatabase } from '../../util/mongodb'
 import styled from 'styled-components'
 import { useState, useEffect } from 'react'
 
-export default function Home({properties, display_final, campaigns_to_show}) {
+export default function Home({properties, display_final, campaigns_to_show, campaigns_saved}) {
 
     const [count, setCount] = useState(0);
+    const [opacity, setOpacity] = useState(0);
+    const [active, setActive] = useState(false);
 
-    
+
     useEffect(() => {
       const interval = setInterval(() => {
-        setCount(count => count + 1);
+        var currentSecond = parseInt(Date(Date.now()).toString().split(" ")[4].split(":")[2]);
         var currentDate = parseInt(Date(Date.now()).toString().split(" ")[4].split(":")[1]);
-      }, 5000);
-      return () => clearInterval(interval);
-    }, []);
 
-  var opacity = 0;
+        if (currentSecond > 30) {
+          setOpacity(0);
+          setActive(active => false);
+        }
+        if (display_final != undefined) {
+          if (display_final.includes(currentDate)) {
+            if (campaigns_to_show != undefined) {
+              if (currentSecond < 30) {
+                if (!active) {
+                  console.log("nao entrou");
+                  var index = campaigns_to_show[Math.floor(Math.random() * campaigns_to_show.length)];
+                  setCount(count => index);
+                  campaigns_to_show.splice(campaigns_to_show.indexOf(index), 1);
+                  setOpacity(1);
+                  setActive(active => true);
+                }
+              }
+            }
+          } else {
+            setOpacity(0);
+            setActive(active => false);
+          }
+        } else {
+          console.log("po crl pt2");
+        }
+        if (currentDate == 59 && currentSecond >= 50) {
+          campaigns_to_show = campaigns_saved.slice(0);
+        }
+      }, 10000);
+      return () => clearInterval(interval); 
+    }, [display_final, campaigns_to_show, active, count, campaigns_saved, opacity]);
+
   if (properties != undefined) {
     if (properties[count] == undefined) {
       setCount(0);
-    } else {
+    } else if (opacity == 1) {
       var left = properties[count].css.left;
       var top = properties[count].css.top;
-      var filepath = properties[count].file.path;
+      if (properties[count].file.path == "none") {
+        var filepath = "";
+        setOpacity(0);
+      } else {
+        var filepath = properties[count].file.path;
+      }
+      console.log(count);
+      console.log(opacity);
+
+    } else {
+      var left = 0;
+      var top = 0;
+      var filepath = "";
     }
     return (
       <div>
@@ -94,9 +136,7 @@ export async function getStaticProps({ params }) {
   
     for (var i = 0; i < total_displays; i++) {
       var random_time = possible_time[Math.floor(Math.random() * possible_time.length)];
-      possible_time.splice(possible_time.indexOf(random_time-1), 1);
       possible_time.splice(possible_time.indexOf(random_time), 1);
-      possible_time.splice(possible_time.indexOf(random_time+1), 1);
       display_times.push(random_time);
     }
     
@@ -104,10 +144,10 @@ export async function getStaticProps({ params }) {
 
     const display_final = JSON.parse(JSON.stringify(display_times_sorted));
 
-    
+    const campaigns_saved = JSON.parse(JSON.stringify(campaigns_to_show));    
 
     return {
-        props: {properties: properties, display_final: display_final, campaigns_to_show: campaigns_to_show },
+        props: {properties: properties, display_final: display_final, campaigns_to_show: campaigns_to_show, campaigns_saved: campaigns_saved },
         revalidate: 1
     };
 }
